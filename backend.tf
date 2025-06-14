340ac32bda382182218d2e23e23a7a847315de18
securify-admin <anavarro+github@securifytechnologies.com>
2025-06-14 13:23:33 -0700

    Update backend.tf with SecurifyTechnologies organization name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   cloud {
-    organization = "your-organization-name"  # Replace with your Terraform Cloud organization name
+    organization = "SecurifyTechnologies"  # Your Terraform Cloud organization name
 
     workspaces {
       name = "palo-aws-poc-v3-gwlb-alb"  # You can customize this workspace name
